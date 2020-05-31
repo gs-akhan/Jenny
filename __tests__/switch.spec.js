0e76e32983076b93dbc442a203b8666d67483030
Azhar Uddin <akhan@gainsight.com>
2020-05-31 12:41:18 +0530

    Added support for comment

--- __tests__/switch.spec.js
+++ __tests__/switch.spec.js
@@ -7,9 +7,10 @@ describe('Testing Switch Statement', () => {
     it('Switch should output 1', () => {
 
         let code = `
+            /* Begin statement */
             (begin 
                 (var x 100)
-                
+                /* This is comment */
                 (switch 
                     ((= x 100) 1)
                     ((= x 200) 2)
