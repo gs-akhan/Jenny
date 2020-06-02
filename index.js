65b2517474a60a4068a3a9bdd059255a33a6ad1c
Azhar Uddin <akhan@gainsight.com>
2020-06-02 11:58:20 +0530

    Added global execution keyword

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var JennyParser = require("./parser/jennyParser");
+var Jenny = require("./Jenny");
+var jenny = new Jenny();
+var fs = require("fs");
+
+
+
+var Jenn
+var args = process.argv;
+let [_node, _cmd, file] = args;
+if (!file) {
+    console.log("File missing to exeute");
+    console.info("> jenny <fileName.jn>");
+    return;
+}
+
+
+var fileToExec =  fs.readFileSync(process.cwd() + "/"+file);
+
+let content = fileToExec.toString();
+
+
+jenny.eval(JennyParser.parse(content));
